f524b19e4dd9fed3b71c986a4020677e00d9832e
Marc Blasco <marcblasconarro@gmail.com>
2022-09-27 10:04:03 +0200

    structuring application in folders (exercice 5.3)

--- ejercicio5/index.js
+++ ejercicio5/index.js
@@ -1,100 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const os= require('os');
+// Enrutadores
+const libros = require(__dirname + '/routers/libros');
+
+// Conectar con BD en Mongo 
+mongoose.connect('mongodb://localhost:27017/libros', 
+    {useNewUrlParser: true, useUnifiedTopology: true});
+
+// Inicializar Express
 let app = express();
-//midleware
+
+// Cargar middleware para peticiones POST y PUT
+// y enrutadores
 app.use(express.json());
+app.use('/', libros);
 
-const Libro = require(__dirname + "/models/libro");
-
-mongoose.connect('mongodb://localhost:27017/libros',
- {useNewUrlParser: true, useUnifiedTopology: true});
-
-
-/*
-//-----------ejercicio 5.1---------------------
-app.get('/', (req, res) => {
-    res.send('Hola, bienvenido/a');
-   });
-
-app.get('/fecha', (req, res) => {
-let now= new Date();
-res.send(now);
-});   
-
-app.get('/usuario', (req, res) => {
-    res.send(os.userInfo().username);
-    });   
-    */
-//---------------final ejercicio 5.1  -----------
-
-//-----------ejercicio 5.2---------------------
-app.get('/libros', (req, res) => {
-    Libro.find().then(resultado => {
-    res.status(200)
-    .send( {ok: true, resultado: resultado});
-    }).catch (error => {
-    res.status(500)
-    .send( {ok: false, error: "Error obteniendo contactos"})
- });
-});
-
-app.get('/libros/:id', (req, res) => {
-    Libro.findById(req.params.id).then(resultado => {
-    if(resultado)
-        res.status(200).send({ok: true, resultado: resultado});
-    else
-        res.status(400).send({ok: false,error: "No se han encontrado contactos"});
-    }).catch (error => {
-    res.status(400).send({ok: false,error: "Error buscando el contacto indicado"});
-    });
-   });
-   
-   app.post('/libros', (req, res) => {
-
-    console.log(req.body)
-    let nuevoLibro = new Libro({
-        titulo : req.body.titulo,
-        editorial : req.body.editorial,
-        precio : req.body.precio
-    });
-    nuevoLibro.save().then(resultado => {
-        res.status(200).send({ok: true, resultado: resultado});
-    }).catch(error => {
-        res.status(400).send({ok: false,error: "Error añadiendo libro"});
-    })
-});
-
-app.put('/libros/:id', (req, res) => {
-
-    Libro.findByIdAndUpdate(req.params.id, {
-        $set: {
-            titulo: req.body.titulo,
-            editorial: req.body.editorial,
-            precio: req.body.precio
-        }
-    }, {new: true}).then(resultado => {
-        if (resultado)
-            res.status(200).send({ok: true, resultado: resultado});
-        else
-            res.status(400).send({ok: false, error: "No se ha encontrado el libro para actualizar"});
-    }).catch(error => {
-        res.status(400).send({ok: false, error:"Error actualizando libro"});
-    });
-});
-   
-app.delete('/libros/:id', (req, res) => {
-    Libro.findByIdAndRemove(req.params.id).then(resultado => {
-        res.status(200).send({ok: true, resultado: resultado});
-    }).catch(error => {
-        res.status(400).send({ok: false, error:"Error eliminando libro"});
-    });
-   });
-
-
-
-
-
-
+// Puesta en marcha del servidor
 app.listen(8080);
