053727f62b1cf71a0c0beec5428f2a6f78c9f0af
Marc Blasco <marcblasconarro@gmail.com>
2022-09-28 10:42:23 +0200

    finishing exercice 6.3

--- ejercicios_tema6/ejercicio6.2/index.js
+++ ejercicios_tema6/ejercicio6.2/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const nunjucks = require('nunjucks');
+const methodOverride = require('method-override');
+
 
 // Enrutadores
 const libros = require(__dirname + '/routers/libros');
@@ -16,11 +18,22 @@ nunjucks.configure('views', {
     autoescape: true,
     express: app
    });
+// motor plantillas
+app.set('view engine', 'njk');
 
 // Cargar middleware para peticiones POST y PUT
 // y enrutadores
-app.set('view engine', 'njk');
+
 app.use(express.json());
+app.use(express.urlencoded());
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      let method = req.body._method;
+      delete req.body._method;
+      return method;
+    } 
+}));
+
 app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
 app.use('/', libros);
 
